bfb678e183c5bb03bccd02e5986bcafe66ba83a4
Sudha Ravi Kumar J <sudha.rkj@gmail.com>
2022-04-09 12:03:34 -0700

    Simplify tree creation and serialization (#4)

--- data_structure/list_node.py
+++ data_structure/list_node.py
@@ -1,11 +1,12 @@
 class ListNode:
     def __init__(self, val=0, next=None, items=None):
-        if items:
-            head = None
-            for item in reversed(items):
-                head = ListNode(item, head)
-            self.val = head.val
-            self.next = head.next
+        if items is not None:
+            if len(items) > 0:
+                head = None
+                for item in reversed(items):
+                    head = ListNode(item, head)
+                self.val = head.val
+                self.next = head.next
         else:
             self.val = val
             self.next = next
